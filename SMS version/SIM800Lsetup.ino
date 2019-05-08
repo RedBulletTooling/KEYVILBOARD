64d2422090b34ab13a3afe6be99d9b3345d021b3
RedBulletTooling <45310844+RedBulletTooling@users.noreply.github.com>
2019-05-08 22:05:02 +0200

    Tool for debugging the SIM800L chip

---
+++ SMS version/SIM800Lsetup.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial SMSSERIAL(8, 9);
+
+void setup() {
+  Serial.begin(115200);
+  SMSSERIAL.begin(57700);
+}
+
+void loop() {
+  if (SMSSERIAL.available()) {
+    Serial.write(SMSSERIAL.read());
+  }
+  
+  if (Serial.available()) {
+    SMSSERIAL.write(Serial.read());
+  }
+}
+
+
+//AT+IPR=57600
+//AT+CMGF=1
+//AT+CNMI=2,2,0,0,0
+//AT&W_SAVE
+//AT&W
